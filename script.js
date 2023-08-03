88993ce340ff152a9f5ebc499daa0d7ea0addef2
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2023-08-03 14:44:03 -0300

    alterações

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 function verifyNumber(){
     var element = document.getElementById('number');
     var number = element.value;
-    
-    if(number % 2 === 0 && number <= 100 && number > 0){
-        alert('O número atende aos critérios.');
-    } else{
-        alert('O número não atende aos critérios.')
+    var resultado = document.querySelector('span');
+
+    if(number === ""){
+        resultado.innerHTML =("Digite um número!");
+    }else if(number % 2 === 0 && number <= 100 && number > 0){
+        resultado.innerHTML =('O número atende aos critérios.');
+    }else{
+        resultado.innerHTML =('O número não atende aos critérios.')
     }
 }
+
