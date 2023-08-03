6565492e913c842d3dcfa931b3a554f661c53eec
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2023-08-03 14:51:53 -0300

    Simbolos

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ function verifyNumber(){
     var resultado = document.querySelector('span');
 
     if(number === ""){
-        resultado.innerHTML =("Digite um número!");
+        resultado.innerHTML =('⚠ Digite um número! ');
     }else if(number % 2 === 0 && number <= 100 && number > 0){
-        resultado.innerHTML =('O número atende aos critérios.');
+        resultado.innerHTML =('☑ O número atende aos critérios.');
     }else{
-        resultado.innerHTML =('O número não atende aos critérios.')
+        resultado.innerHTML =('☒ O número não atende aos critérios.')
     }
 }
 
