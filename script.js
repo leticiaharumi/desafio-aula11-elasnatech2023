3ac825f59dd600d45cd87e38537d480d9cd46585
Leticia Harumi <80927546+leticiaharumi@users.noreply.github.com>
2023-08-09 19:40:39 -0300

    alteração de condição

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function verifyNumber(){
 
     if(number === ""){
         resultado.innerHTML =('⚠ Digite um número! ');
-    }else if(number % 2 === 0 && number <= 100 && number > 0){
+    }else if(number % 2 === 0 && number < 100 && number > 0){
         resultado.innerHTML =('☑ O número atende aos critérios.');
     }else{
         resultado.innerHTML =('☒ O número não atende aos critérios.')
